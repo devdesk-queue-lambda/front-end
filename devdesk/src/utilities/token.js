3f6ccd8facd7160103cc6e0a8e6714a3cd04835c
Dustin Hamano <dhamano@Dustins-iMac-Pro-2.local>
2019-06-24 23:09:51 -0700

    login with submit. built out new ticket form. navigation items show/hide based on login state

--- devdesk/src/utilities/token.js
+++ devdesk/src/utilities/token.js
@@ -1,6 +1,6 @@
 import { LOGIN_SUCCESS } from '../actions';
 
 export const setToken = store => next => action => {
-  action.type === LOGIN_SUCCESS && localStorage.setItem('userToken', action.payload.token);
+  action.type === LOGIN_SUCCESS && localStorage.setItem('token', action.payload);
   next(action);
 }
