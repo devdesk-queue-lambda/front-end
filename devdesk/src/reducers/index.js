2666d1281e04d78a4063476a225c6dec88ea02af
Dustin <clerin+gitkraken@gmail.com>
2019-06-26 10:53:46 -0700

    added delete and reset to update ticket. added reset to new ticket. commented out na

--- devdesk/src/reducers/index.js
+++ devdesk/src/reducers/index.js
@@ -3,10 +3,12 @@ import { login } from './login';
 import { register } from './register';
 import { newTicket } from './newticket';
 import { updateticket } from './updateticket';
+import { deleteticket } from './delete';
 
 export default combineReducers({
   login,
   register,
   newTicket,
-  updateticket
+  updateticket,
+  deleteticket
 });
